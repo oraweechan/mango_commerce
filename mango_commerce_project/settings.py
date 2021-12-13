bcc4e1c49d2761aa9837b753a5c4c8cd1d3e8cfb
“Orawee <oraweechan@gmail.com>
2021-12-13 14:01:55 -0800

    create and run migrations for user

--- mango_commerce_project/settings.py
+++ mango_commerce_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken'
 ]
 
-AUTH_USER_MODEL = 'api.User'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -134,3 +134,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'api.User'
